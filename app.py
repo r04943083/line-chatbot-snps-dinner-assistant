4ee0c4e6de19a192fd45952d0e15bd8dd5296055
r04943083 <55845049+r04943083@users.noreply.github.com>
2019-09-27 11:35:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,42 +6,37 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('jgex4RHeYTbbBBwCOvMNB8X4mrM/ZHWShjsrqmjDra43C5knZBPW7O8rMBDU8O24RTAFm+V78G504OO6J/i+XQ78lgwYVDDO6o1lmf+P544hW/i33/Zkqq0aLdrhKcOwKatp7oCm8aw0bYExRcMGtgdB04t89/1O/w1cDnyilFU=
-')
-handler = WebhookHandler('d57f5ce688eaf2c98c0d54468bbfbfbc')
+# Channel Access Token
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+# Channel Secret
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
-
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
